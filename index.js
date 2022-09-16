7dbbe53ea6e72801fd0fc25f3f308c424849c4d6
Praveen Sangroula <46919290+praveen8954@users.noreply.github.com>
2022-09-17 01:29:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 let popup=document.getElementById('popup')
 let cross=document.getElementById('cross')
+let body=document.getElementById('body')
 console.log(popup)
 // popup.style.display
 function opens(event){
    setTimeout(popup.style.display='flex',20000)
+   body.setAttribute('class','blur')
+   popup.style.display='flex'
  }
 window.addEventListener('load',opens  )
 function close(event){
    popup.style.display='none'
+   body.setAttribute('class','null')
  }
 cross.addEventListener('click',close  )
  
